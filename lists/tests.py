39e8596f6e22495cc47e173d1b0cb6f94985afb9
sumit-linux <sumit@sumit-linux>
2014-11-18 01:52:45 +0530

    basic view  and minimal html

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 
@@ -8,6 +10,13 @@ class HomePageTest(TestCase):
     def test_root_url_resolve_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func,home_page)
+
+    def test_home_page_url_returns_correct_html(self):
+        req = HttpRequest()
+        res = home_page(req)
+        self.assertTrue(res.content.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>',res.content)
+        self.assertTrue(res.content.endswith('</html>'))
 
 
 
