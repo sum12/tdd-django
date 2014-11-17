53c3b903b0f53d211ec302327b67de17edacd7e0
sumit-linux <sumit@sumit-linux>
2014-11-18 02:16:35 +0530

    functional test now adds a new item and checks in table

--- functional_test.py
+++ functional_test.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys  import  Keys
+
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(5)
 
     def tearDown(self):
         self.browser.quit()
@@ -11,6 +14,23 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('localhost:8888')
         self.assertIn('To-Do',self.browser.title)
+        headerText = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn(headerText,'To-Do')
+
+        input_box = self.browser.find_element_id('id_new_item')
+        self.assertEqual(
+                    input_box.get_attribute('placeholder'),
+                    'Enter a new To-Do Item'
+                )
+        input_box.sendkeys('Buy Peacock Feathers')
+        input_box.sendkeys(Keys.Enter)
+
+        table = self.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertTrue(
+                    any(row == '1: Buy Peacock Feathers' for row in rows)
+                )
         self.fail('Finish the test')
 
 
