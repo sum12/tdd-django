2ea2439372fcb88af28352248e5180d7788d2918
sumit-linux <sumit@sumit-linux>
2014-11-20 01:56:23 +0530

    added template, functional Test and test

--- functional_test.py
+++ functional_test.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_taag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('localhost:8000')
         self.assertIn('To-Do',self.browser.title)
@@ -29,11 +34,9 @@ class NewVisitorTest(unittest.TestCase):
         input_box.send_keys('Use Peacock Feathers to make a fly')
         input_box.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        self.check_for_row_in_list_table('1: Buy Peacock Feathers')
+        self.check_for_row_in_list_table('2: Use Peacock Feather to make a fly')
 
-        self.assertIn('1: Buy Peacock Feathers', [row.text for row in rows])
-        self.assertIn('2: Use Peacock Feathers to make a fly', [row.text for row in rows])
         self.fail('Finish the test')
 
 
