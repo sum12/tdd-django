279f6e077986a11aaf635f458ca4141942cdf409
sumitj <sumitjami@gmail.com>
2014-11-20 22:44:08 +0530

    functional test as django test, giving a clean slate eveytime a test start

--- functional_test/test.py
+++ functional_test/test.py
@@ -1,9 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys  import  Keys
 
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(5)
@@ -17,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('localhost:8000')
+        self.browser.get(self.live_server_url)
         self.browser.implicitly_wait(10)
         self.assertIn('To-Do',self.browser.title)
         headerText = self.browser.find_element_by_tag_name('h1').text
