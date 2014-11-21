feb03e27b1b7f2456fc0e89d418a421ec7fc4b6e
sumitj <sumitjami@gmail.com>
2014-11-21 09:26:02 +0530

    intermediate commit, added in list model and written down FT, unittests on the way

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item,List
 
 class HomePageTest(TestCase):
 
@@ -30,23 +30,32 @@ class HomePageTest(TestCase):
 
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = "The First (Ever) List Item"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = "The Second Item"
+        second_item.list = list_
         second_item.save()
 
         saved_objects = Item.objects.all()
         self.assertEqual(saved_objects.count(),2)
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list,list_)
 
         first_saved_item = saved_objects[0]
         second_saved_item = saved_objects[1]
 
         self.assertEqual(first_saved_item.text,'The First (Ever) List Item')
+        self.assertEqual(first_saved_item.list,list_)
+        self.assertEqual(second_saved_item.list,list_)
         self.assertEqual(second_saved_item.text,'The Second Item')
 
 class ListViewTest(TestCase):
